b1b8192044b8bf8763aa90edd3eca6dfc2b86188
ranasarkar90 <mdranasarkar909@gmail.com>
2023-04-03 23:13:18 +0600

    java class-2

---
+++ app.js
@@ -0,0 +1,64 @@
+// function nam() {
+//   let n =10;
+//   let m =20;
+//   let sum = n+m;
+
+//   console.log("sum:", sum);
+// }
+// nam()
+
+
+// function nam(n,m) {
+//   let sum = n+m;
+//   console.log("sum:", sum);
+// }
+// nam(10,20);
+// nam(100,200);
+// nam(1000,300);
+
+
+
+// function nam(n,m) {
+//   let result = n+m;
+//   return result; 
+// }
+// let n1 =nam(10,20);
+// let n2 =nam(100,200);
+// let n3 =nam(1000,300);
+// let n4 =nam(50,30);
+
+// console.log(n1,n2,n3,n4);
+
+
+
+// function sum(nam) {
+//   let result = "Mr. "+nam;
+//   return result;
+// }
+// console.log
+// let nam1 =sum("rana")
+
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(3,4);
+add(2,9);
+
+
+function greet() {
+  console.log("Hello bangladesh");
+}
+greet();
+
+
+let a = "hello";
+function nam3 () {
+    console.log(a);
+}
+nam3();
+
+
+
+
